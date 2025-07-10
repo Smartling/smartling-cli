282b05c5ba3418ba4d519f436bb9718b38a405ed
az-smartling <azavodovskyy@smartling.com>
2025-07-10 08:17:29 +0200

    changed to RunE

--- cmd/docs/cmd_docs.go
+++ cmd/docs/cmd_docs.go
@@ -1,9 +1,8 @@
 package docs
 
 import (
+	"fmt"
 	"github.com/Smartling/smartling-cli/services/helpers/rlog"
-	"os"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
@@ -13,13 +12,13 @@ func NewDocsCmd() *cobra.Command {
 	docsCmd := &cobra.Command{
 		Use:   "docs",
 		Short: "Generate markdown docs for CLI commands",
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			err := doc.GenMarkdownTree(cmd.Root(), "./docs")
 			if err != nil {
-				rlog.Errorf("failed to generate docs: %v", err)
-				os.Exit(1)
+				return fmt.Errorf("failed to generate docs: %v", err)
 			}
 			rlog.Infof("markdown docs generated in ./docs/")
+			return nil
 		},
 	}
 
